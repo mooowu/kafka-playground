c1faa628306623b364f2b77acd134f4e7407ed48
mooowu <me@mooowu.xyz>
2025-07-12 15:14:38 +0900

    feat: EC2 인스턴스 타입을 t2.medium으로 변경

--- main.go
+++ main.go
@@ -233,7 +233,7 @@ func main() {
 
 		for i := 0; i < 7; i++ {
 			instance, err := ec2.NewInstance(ctx, fmt.Sprintf("kafka-playground-instance-%d", i), &ec2.InstanceArgs{
-				InstanceType: pulumi.String("t2.micro"),
+				InstanceType: pulumi.String("t2.medium"),
 				Ami:          pulumi.String(ami.Id),
 				SubnetId:     privateSubnets[i%len(privateSubnets)].ID(),
 				VpcSecurityGroupIds: pulumi.StringArray{
